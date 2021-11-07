a665b3321a5266b95cbcd264b27b49e2177bb976
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-07 13:23:12 -0600

    comparative benchmarks FBD 1.5-2.5x slower

--- benchmarks/kronecker.jl
+++ benchmarks/kronecker.jl
@@ -1,9 +1,27 @@
-using FBD
+using FBD, Plots, Unzip
 include("../external/hyperkron.jl")
 
-#external
-A,hedges = hyperkron_graph(kron_params(0.99, 0.2, 0.3, 0.05), 5)
+function time_both(power)
+    #external
+    a = @elapsed A,hedges = hyperkron_graph(kron_params(0.99, 0.2, 0.3, 0.05), power)
 
-#internal
-sampler = Kronecker_sampler(rand(5,5,5), 7)
-edges = rand(sampler, 100)
+    edge_count = length(hedges[1])
+
+    #internal
+    b = @elapsed sampler = Kronecker_sampler(kron_params(0.99, 0.2, 0.3, 0.05), power)
+    c = @elapsed edges = rand(sampler, edge_count)
+
+    edge_count,a,b+c,b,c
+end
+
+data = unzip((power, minimum.(unzip(time_both(power) for trial in 1:5))...) for power in 1:12)
+plot(data[1], log10.(data[2]), label="edges (log10)", legend=(.15,.95), title="runtime vs k")
+offset = log(minimum(minimum.(data)))
+plot!(data[1], data[3] ./ data[2] * 1e6, label="external (μs per edge)")
+plot!(data[1], data[4] ./ data[2] * 1e6, label="internal (μs per edge)")
+plot!(data[1], data[5] ./ data[2] * 1e6, label="create sampler (μs per edge)")
+plot!(data[1], data[6] ./ data[2] * 1e6, label="sample (μs per edge)")
+plot!(data[1], data[4]./data[3], label="internal/external")
+
+scatter(data[2], data[3], label="external", legend=(.15,.95), title="runtime vs edges")
+scatter!(data[2], data[4], label="internal")
