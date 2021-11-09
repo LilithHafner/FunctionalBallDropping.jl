e18c5c8843491da28bbbb37a3a09a8d0494ab555
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-08 22:03:00 -0600

    minor benchmark ploting and CI

--- benchmarks/kronecker.jl
+++ benchmarks/kronecker.jl
@@ -11,17 +11,27 @@ function time_both(power)
     b = @elapsed sampler = Kronecker_sampler(kron_params(0.99, 0.2, 0.3, 0.05), power)
     c = @elapsed edges = rand(sampler, edge_count)
 
-    edge_count,a,b+c,b,c
+    @assert length(edges) == edge_count
+
+    #internal 2
+    #d = @elapsed sampler = Kronecker_sampler(kron_params(0.99, 0.2, 0.3, 0.05), power; size=edge_count÷10)
+    #e = @elapsed edges = rand(sampler, edge_count)
+
+    #@assert length(edges) == edge_count
+
+    edge_count,a,b+c,b,c#,d+e,d,e
 end
 
-data = unzip((power, minimum.(unzip(time_both(power) for trial in 1:5))...) for power in 1:12)
-plot(data[1], log10.(data[2]), label="edges (log10)", legend=(.15,.95), title="runtime vs k")
-offset = log(minimum(minimum.(data)))
+data = unzip((power, minimum.(unzip(time_both(power) for trial in 1:3))...) for power in 1:15)
+plot(data[1], log10.(data[2]), label="edges (log10)", legend=(.15,.95), style=:dash, color=:black, title="runtime vs k")
 plot!(data[1], data[3] ./ data[2] * 1e6, label="external (μs per edge)")
 plot!(data[1], data[4] ./ data[2] * 1e6, label="internal (μs per edge)")
 plot!(data[1], data[5] ./ data[2] * 1e6, label="create sampler (μs per edge)")
 plot!(data[1], data[6] ./ data[2] * 1e6, label="sample (μs per edge)")
-plot!(data[1], data[4]./data[3], label="internal/external")
+#plot!(data[1], data[4+3] ./ data[2] * 1e6, label="2 internal (μs per edge)")
+#plot!(data[1], data[5+3] ./ data[2] * 1e6, label="2 create sampler (μs per edge)")
+#plot!(data[1], data[6+3] ./ data[2] * 1e6, label="2 sample (μs per edge)")
+display(plot!(data[1], data[3] ./ data[4], style=:dot, color=:grey, label="ratio: external/internal"))
 
-scatter(data[2], data[3], label="external", legend=(.15,.95), title="runtime vs edges")
-scatter!(data[2], data[4], label="internal")
+plot(data[2], data[3], label="external", marker=:cirlce, legend=(.15,.95), title="runtime vs edges")
+display(plot!(data[2], data[4], label="internal", marker=:cirlce))
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,9 @@ using Test
 include("dchsbm/model.jl")
 include("kronecker/util.jl")
 #include("kronecker/model.jl") these tests are waiting on https://github.com/bramtayl/Unzip.jl/issues/7
-printstyled("Some tests must be run manually with: include(\"test/kronecker/model.jl\")\n"; color=Base.warn_color())
+printstyled("Kronecker model tests must be run manually with: include(\"test/kronecker/model.jl\")\n\n"; color=Base.warn_color())
+
+#include("../benchmarks/kronecker.jl")
+printstyled("Benchmarks are excluded from CI testing because they use plotting which is _slow_ to load :(
+and because they have dependencies which fail in the test harness. Please test manually with:
+include(\"benchmarks/kronecker.jl\")\n"; color=Base.warn_color())
