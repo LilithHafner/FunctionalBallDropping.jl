a551f8d047358fbd9ad62769e76887716cdb5629
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-15 09:29:03 -0600

    update test project management to 1.2+; remove broken Unzip dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,9 @@ include("dchsbm/model.jl")
 include("hyper_pa.jl")
 
 include("kronecker/util.jl")
-#include("kronecker/model.jl") these tests are waiting on https://github.com/bramtayl/Unzip.jl/issues/7
-printstyled("Kronecker model tests must be run manually with: include(\"test/kronecker/model.jl\")\n\n"; color=Base.warn_color())
+include("kronecker/model.jl")
 
 #include("../benchmarks/kronecker.jl")
 printstyled("Benchmarks are excluded from CI testing because they use plotting which is _slow_ to load :(
-and because they have dependencies which fail in the test harness. Please test manually with:
+Please test manually with:
 include(\"benchmarks/kronecker.jl\")\n"; color=Base.warn_color())
