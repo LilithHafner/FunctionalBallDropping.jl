5bc58dbad64e13ff9ca77e0c3554f68b387e1d69
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-16 00:58:56 -0600

    Test mbps (incl. regression) (#10)

--- test/examples.jl
+++ test/examples.jl
@@ -1,7 +1,11 @@
 @testset "examples" begin
-    for size in [30, 1000, 100_000]
+    for size in [3, 792, 40_000]
         for generator in [DCHSBM_sampler, Kronecker_sampler, hyper_pa]
             @test example(generator, size) isa Vector{Vector{Int}}
         end
     end
+    speeds = FBD.MBPS(size=10_000, trials=6)
+    @test length(speeds) == 3
+    display(speeds)
+    @test all(last.(speeds) .>= 3)
 end
