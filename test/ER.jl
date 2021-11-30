834c8e7f1118d24a21eb64d2f4084337862fd580
caholdener01 <84983684+caholdener01@users.noreply.github.com>
2021-11-30 10:06:46 -0600

    Add files via upload

---
+++ test/ER.jl
@@ -0,0 +1,18 @@
+using Test
+
+N = 100
+kmax = 3
+P = 0.001
+sampler = ER_sampler(N, kmax, P)
+
+graph = rand(sampler)
+
+for i in graph
+    println(i)
+end
+
+@testset "shape" begin
+    @test length(graph) == floor(sampler.expected_edges) || length(graph) == ceil(sampler.expected_edges)
+    @test all(length.(graph) .== kmax)
+    @test sampler.expected_edges == (binomial((N+kmax-1), kmax) * P)
+end
