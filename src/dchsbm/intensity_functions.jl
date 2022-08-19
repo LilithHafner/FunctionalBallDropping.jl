0d66b254b18e037c4232bd452678f4bedc2fa08c
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-08-19 10:44:19 -0500

    document and simplify intensity functions

--- src/dchsbm/intensity_functions.jl
+++ src/dchsbm/intensity_functions.jl
@@ -1,4 +1,18 @@
 sorted_unique_count(m) = isempty(m) ? 0 : 1+sum(m[2:end].≠m[1:end-1])
 
+"""
+    inverse_power_intensity_function(α)
+
+Returns an intensity function that is of the form ``n^{-α}`` where ``n`` is the number of
+distinct community groups a multiset contains. Input multisets must be sorted.
+"""
 inverse_power_intensity_function(α::Real) = m -> 1/sorted_unique_count(m)^α
-all_or_nothing_intensity_function(α::Real, β::Real) = m -> all(i -> i == first(m), m) ? α : β
+
+"""
+    all_or_nothing_intensity_function(α, β)
+
+Returns an intensity function that is `α` when a multiset contains only a single community
+and `β` otherwise.
+"""
+all_or_nothing_intensity_function(α::Real, β::Real) = m -> all(isequal(first(m)), m) ? α : β
+# not using allequal because it was introduced in 1.8.
