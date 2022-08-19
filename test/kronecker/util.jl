1a1a435213851cc8b4c731a29c42386f5f43d99e
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-08-19 10:33:57 -0500

    test that kronecker power throws on negative exponent

--- test/kronecker/util.jl
+++ test/kronecker/util.jl
@@ -10,4 +10,5 @@ end
     @test p1 === a
     @test p2 == [0 0 0 4; 0 0 6 8; 0 6 0 8; 9 12 12 16]
     @test reduce(FunctionalBallDropping.kronecker_product, [p0, p1, p0, p0, p1]) == p2
+    @test_throws ArgumentError FunctionalBallDropping.kronecker_power(p1, -1)
 end
